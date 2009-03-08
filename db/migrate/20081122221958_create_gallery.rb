d632f79ace79829915575d14415e395a57b24d2c
Ian Enders <ian.enders@gmail.com>
2009-03-08 00:16:58 -0500

    Removing some files.

--- db/migrate/20081122221958_create_gallery.rb
+++
@@ -1,7 +0,0 @@
-class CreateGallery < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20081122222002_create_image.rb
+++
@@ -1,7 +0,0 @@
-class CreateImage < ActiveRecord::Migration
-  def self.up
-  end
-
-  def self.down
-  end
-end
