ed8576b94b3131be84f2d4671c828609b701a599
Ana Carter <anacarter@anas-macBook-2.gateway.2wire.net>
2009-03-30 00:33:34 -0400

    Styling on text

--- public/javascripts/sifr.js
+++ public/javascripts/sifr.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
   $.sifr({ path: '/swf/', save: true });
 	$('.blog_content h3').sifr({
 		font: 'tuffy',
-		fontSize: '26pt',
+		fontSize: '30px',
 		color: '#464646'
 	});
 	$('.nav_links div').sifr({
 		font: 'tuffy',
-		fontSize: '18pt',
-		color: '#000',
-		link: '#000',
+    fontSize: '17px',
+		color: '#000000',
+		link: '#000000',
 		hover: '#FD8340'
 	});
 	$('.footer_links div').sifr({
 		font: 'tuffy_bold',
-		fontSize: '16px',
-		color: '#000',
-		link: '#000',
+		fontSize: '14px',
+		color: '#000000',
+		link: '#000000',
 		hover: '#FD8340'
 	});
 });
