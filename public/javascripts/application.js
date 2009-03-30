fb13a2907d00b709fcabfdf36c57d41fc0ffbf5e
Ian Enders <ian.enders@gmail.com>
2009-03-29 21:17:52 -0400

    Stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
   $.sifr({ path: '/swf/', save: true });
 	$('.blog_content h3').sifr({
 		font: 'tuffy',
-		fontSize: '30px',
+		fontSize: '26pt',
 		color: '#454545'
 	});
-	// $('#navigation ul li').sifr({
-	// });
+	$('.nav_links div').sifr({
+		font: 'tuffy',
+		fontSize: '18pt',
+		color: '#000000',
+		link: '#000000',
+		hover: '#FD8340'
+	});
 });
