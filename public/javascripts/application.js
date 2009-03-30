01073fdec261158a0268d9d6ccfb92064e86a294
Ian Enders <ian.enders@gmail.com>
2009-03-29 20:16:52 -0400

    Sifr

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
   $.sifr({ path: '/swf/', save: true });
-	$('h3').sifr({
+	$('.blog_content h3').sifr({
 		font: 'tuffy',
 		fontSize: '30px',
 		color: '#454545'
 	});
-	$('#navigation ul li').sifr({
-		font: 'tuffy',
-		fontSize: '18px',
-		color: 'black'
-	});
+	// $('#navigation ul li').sifr({
+	// });
 });
