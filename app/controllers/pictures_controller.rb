007c1347ce43047a91cc95535fed6f1a6b4bb0f2
Ian Enders <ian.enders@gmail.com>
2009-04-25 16:18:55 -0400

    Image changes.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,5 +7,9 @@ class PicturesController < ApplicationController
   def album
     render :partial => 'album', :object => Album.find(params[:id]), :locals => { :num_image_rows => params[:num_image_rows].to_i }
   end
+  
+  def photo
+    render :partial => 'photo', :object => Photo.find(params[:id])
+  end
 
 end
