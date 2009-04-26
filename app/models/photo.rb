eec06588585862592d13ae7a85c0997c2310076e
Ian Enders <ian.enders@gmail.com>
2009-04-25 23:54:34 -0400

    Lots of gallery enhancements.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -30,15 +30,35 @@ class Photo < ActiveRecord::Base
   validates_as_attachment
   
   protected   
-  def resize_image(img, size) 
-    logger.debug "resizing image"
+  def resize_image(img, size)
+    original_exif = MiniExiftool.new(img.filename)
     if (size.is_a?(String) && size =~ /^crop: (\d*)x(\d*)/i) || (size.is_a?(Array) && size.first.is_a?(String) && size.first =~ /^crop: (\d*)x(\d*)/i)
-      logger.debug "resizing image has crop in it"
       img.crop_resized!($1.to_i, $2.to_i)  
       self.temp_path = write_to_temp_file(img.to_blob)  
     else  
       super
-    end  
+    end
+      
+      
+    if size == "608>x456"
+      # Copy original exif data into resized image.
+      # Do a two pass save if it doesnt work the first time: we remove all attributes which caused the save to fail.
+      resized_exif = MiniExiftool.new(self.temp_path)
+      MiniExiftool.writable_tags.each do |tag|
+        if !original_exif[tag].blank?
+          resized_exif[tag] = original_exif[tag]
+        end
+      end
+      if !resized_exif.save
+        resized_exif.revert
+        MiniExiftool.writable_tags.reject {|t| resized_exif.errors[t] }.each do |tag|
+          if !original_exif[tag].blank?
+            resized_exif[tag] = original_exif[tag]
+          end
+        end
+        resized_exif.save
+      end
+    end
   end
   
 end
